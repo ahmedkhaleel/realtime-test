8eb8e1df5d08354735cb474155c2ac1160c03be8
ahmedkhaleel <duZhuqkyptu8barnuz>
2021-04-23 00:11:15 +0200

    favtory and seed

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Question::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+
+        return [
+            'title'=>$this->faker->sentence,
+            'slug'=>$this->faker->sentence,
+            'body'=>$this->faker->text,
+            'category_id'=>function(){
+            return Category::all()->random();
+            },
+            'user_id'=>function(){
+            return User::all()->random();
+            }
+
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\like;
+use App\Models\Question;
+use App\Models\Reply;
+use App\Models\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +19,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         // \App\Models\User::factory(10)->create();
+       User::factory(10)->create();
+       Category::factory(10)->create();
+       Question::factory(5)->create();
+       Reply::factory(50)->create()->each(function($reply){
+           return $reply->Like()->save(like::factory(10)->make());
+       });
+
+
+
+
     }
 }
